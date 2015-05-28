90d6cfae8c6a0f22d99a16a13d423db187c0ac88
Liam Mencel <lamencel@gmail.com>
2015-05-28 18:45:47 +0300

    Checklist
    Started checklist, using Country example

---
+++ android/orientation_app/app/src/main/java/kaust/orientationapp/Country.java
@@ -0,0 +1,36 @@
+package kaust.orientationapp;
+
+public class Country {
+
+    String code = null;
+    String name = null;
+    boolean selected = false;
+
+    public Country(String code, String name, boolean selected) {
+        super();
+        this.code = name;
+        //this.name = code;
+        this.selected = selected;
+    }
+
+    public String getCode() {
+        return code;
+    }
+    public void setCode(String code) {
+        this.code = code;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isSelected() {
+        return selected;
+    }
+    public void setSelected(boolean selected) {
+        this.selected = selected;
+    }
+
+}
--- android/orientation_app/app/src/main/java/kaust/orientationapp/CustomAdapter.java
+++ android/orientation_app/app/src/main/java/kaust/orientationapp/CustomAdapter.java
@@ -1,6 +1,7 @@
 package kaust.orientationapp;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -8,6 +9,7 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class CustomAdapter extends BaseAdapter{
 
@@ -73,6 +75,11 @@ public class CustomAdapter extends BaseAdapter{
             @Override
             public void onClick(View v) {
                 // TODO Auto-generated method stub
+                Toast.makeText(context, "You Clicked " + result[position], Toast.LENGTH_LONG).show();
+                if (result[position].equals("Checklist")) {
+                    Intent intent = new Intent(context, Checklist.class);
+                    context.startActivity(intent);
+                }
             }
         });
 
