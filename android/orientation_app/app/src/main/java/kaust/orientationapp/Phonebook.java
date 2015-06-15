44defe74d6dd8d9a50e47b399326f88d1b5d1139
Liam Mencel <lamencel@gmail.com>
2015-06-15 18:09:10 +0300

    minor phonebook changes
    minor phonebook changes

--- android/orientation_app/app/src/main/java/kaust/orientationapp/Phonebook.java
+++ android/orientation_app/app/src/main/java/kaust/orientationapp/Phonebook.java
@@ -1,36 +1,48 @@
 package kaust.orientationapp;
 
 
+import android.content.res.Resources;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.widget.ListView;
 
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Phonebook extends ActionBarActivity {
 
     private ListView lvPhone;
+    public String[] ListContacts = new String[]{};
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phonebook);
 
-        lvPhone = (ListView)findViewById(R.id.listPhone);
 
+        try {
+            Resources ResFiles = getResources();
+            InputStream ReadDbFile = ResFiles.openRawResource(R.raw.contacts);
+            byte[] Bytes = new byte[ReadDbFile.available()];
+            ReadDbFile.read(Bytes);
+            String DbLines = new String(Bytes);
+            ListContacts = DbLines.split("\n"); // Split the content by line
+        } catch (Exception e) {
+        }
+
+        lvPhone = (ListView)findViewById(R.id.listPhone);
         List<Contact> listContact = new ArrayList<>();
-        listContact.add(new Contact(
-                BitmapFactory.decodeResource(getResources(), R.drawable.avatar_pete),
-                "Pete Houston", "010-9817-6331", "pete.houston.17187@gmail.com"));
-        listContact.add(new Contact(
-                BitmapFactory.decodeResource(getResources(), R.drawable.avatar_lina),
-                "Lina Cheng", "046-7764-1142", "lina.cheng011@sunny.com"));
-        listContact.add(new Contact(
-                BitmapFactory.decodeResource(getResources(), R.drawable.avatar_jenny),
-                "Jenny Nguyen", "0913-223-498", "jenny_in_love98@yahoo.com"));
+        for (String contact : ListContacts){
+            String[] fields = contact.split(",");
+            listContact.add(new Contact(
+                    BitmapFactory.decodeResource(getResources(), R.drawable.personal),
+                    fields[0], fields[1], fields[2]));
+        }
+
         PhonebookAdapter adapter = new PhonebookAdapter(this, listContact);
         lvPhone.setAdapter(adapter);
+
     }
 }
