c81f91c5f41eea39ac7669904f4d6e8f1baa8d9a
Liam Mencel <lamencel@gmail.com>
2015-08-05 21:40:51 +0300

    Added points of interest to map #19
    Travel maps now has points of interest

--- android/orientation_app/app/src/main/java/kaust/orientationapp/TravelMaps.java
+++ android/orientation_app/app/src/main/java/kaust/orientationapp/TravelMaps.java
@@ -1,16 +1,49 @@
 package kaust.orientationapp;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 
+import java.io.InputStream;
+
+import kaust.orientationapp.maps.MWMPoint;
 import kaust.orientationapp.maps.MapsWithMeApi;
 
 public class TravelMaps extends ActionBarActivity {
 
+    void showMultiplePoints()
+    {
+        // Open tab separated file
+        String[] ListPoi = null;
+        try {
+            Resources ResFiles = getResources();
+            InputStream ReadDbFile = ResFiles.openRawResource(R.raw.poi);
+            byte[] Bytes = new byte[ReadDbFile.available()];
+            ReadDbFile.read(Bytes);
+            String DbLines = new String(Bytes);
+            ListPoi = DbLines.split("\n"); // Split the content by line
+        } catch (Exception e) {
+        }
+
+        // Convert objects to MMWPoints
+        final MWMPoint[] points = new MWMPoint[ListPoi.length];
+        for (int i = 0; i < ListPoi.length; i++)
+        {
+            String[] str = ListPoi[i].split("\t");
+            float lat = Float.valueOf(str[2]);
+            float lon = Float.valueOf(str[3]);
+            String name = str[0];
+            points[i] = new MWMPoint(lat, lon, name);
+        }
+        // Show all point on the map, you could also provide some title
+        MapsWithMeApi.showPointsOnMap(this, "Jeddah points of interest", points);
+    }
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.travel_maps);
+        //MapsWithMeApi.showPointOnMap(this, 22.31132, 39.10270, "Student Centre");
+        showMultiplePoints();
 
-        MapsWithMeApi.showPointOnMap(this, 22.31132, 39.10270, "Student Centre");
     }
 }
